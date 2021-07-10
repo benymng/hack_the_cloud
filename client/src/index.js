323224d94f6d103ab32fc15ec6d49f9fbbb5fe8b
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-07-10 11:24:47 -0600

    Material-UI install + theme setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
+import "./styles/index.css";
+
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/index.css";
+
+import { ThemeProvider } from "@material-ui/core/styles";
+
 import App from "./App";
+import theme from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
