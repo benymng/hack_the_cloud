64eb805d96afce7ac3f4737c84c06ea978484bf1
Benjamin Ng <benymng@gmail.com>
2021-07-10 13:14:00 -0600

    vision added

---
+++ server/functions/vision.py
@@ -0,0 +1,40 @@
+import io
+import os
+
+def load_food_name():
+    names = [line.rstrip('\n').lower() for line in open("fruits.dict")]
+    return names
+
+def recognize_food(img_path, list_foods):
+    # Imports the Google Cloud client library
+    from google.cloud import vision
+        
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
+
+    # The name of the image file to annotate
+    file_name = os.path.abspath(img_path)
+
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    print("Descriptions: ")
+    for label in labels:
+        description = label.description.lower()
+        if (description in list_foods):
+            print (description)
+    
+
+recognize_food("./pictures/blue guys.jpg", load_food_name())
+
+
+
+
+
