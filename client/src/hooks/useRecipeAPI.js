3c00d336ebfb090fb9e6180c4e5869a8f521aebf
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-07-11 00:01:51 -0600

    Created callAPI function and added fetchRecipe

--- client/src/hooks/useRecipeAPI.js
+++ client/src/hooks/useRecipeAPI.js
@@ -1,23 +1,38 @@
 const useRecipeAPI = () => {
+  const callAPI = async (endpoint = "", data = {}, method = "POST") => {
+    const baseURL = "https://captain-cook-api.herokuapp.com";
+
+    const response = await fetch(`${baseURL}/${endpoint}`, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const responseData = await response.json();
+
+    return responseData;
+  };
+
   const searchAllRecipes = async (query, callback) => {
     const data = {
-      query: query,
+      query,
     };
 
-    const response = await fetch(
-      "https://captain-cook-api.herokuapp.com/search",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }
-    );
-
-    const recipes = await response.json();
+    const recipes = await callAPI("search", data);
 
     callback(recipes);
+  };
+
+  const fetchRecipe = async (recipeHref, callback) => {
+    const data = {
+      recipeHref,
+    };
+
+    const recipe = await callAPI("recipe", data);
+
+    callback(recipe);
   };
 
   return {
