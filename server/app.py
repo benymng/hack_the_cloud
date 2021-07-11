df60458377dcec823c3c1c98ee5bbbf1784c8f61
Benjamin Ng <benymng@gmail.com>
2021-07-11 15:56:26 -0600

    trying to fix socket

--- server/app.py
+++ server/app.py
@@ -98,8 +98,7 @@ def socket():
     
 
 if __name__ == "__main__":
-    print('[INFO] Starting server at http://localhost:5001')
-    socketio.run(app=app, host='0.0.0.0', port=5001)
+    socketio.run(app)
     # app.run()
 
 
