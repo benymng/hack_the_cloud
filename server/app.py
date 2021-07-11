a73e16d1dd0ab25b1b4bf4cb3a029ccdf320fcdc
Benjamin Ng <benymng@gmail.com>
2021-07-11 14:28:40 -0600

    commented out text to speech part

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, make_response, jsonify, request
 from flask_cors import CORS
-from google.cloud import texttospeech
+# from google.cloud import texttospeech
 from flask_socketio import SocketIO
 
 from functions import scrape, vision
@@ -60,36 +60,36 @@ def recipe():
 
     return make_response(jsonify(recipe_dict), 200)
 
-app.route("/text-to-speech", methods=["POST"])
-def textToSpeech():
-    # Instantiates a client
-    client = texttospeech.TextToSpeechClient()
+# app.route("/text-to-speech", methods=["POST"])
+# def textToSpeech():
+#     # Instantiates a client
+#     client = texttospeech.TextToSpeechClient()
 
-    # Set the text input to be synthesized
-    synthesis_input = texttospeech.SynthesisInput(text="Hello,!")
+#     # Set the text input to be synthesized
+#     synthesis_input = texttospeech.SynthesisInput(text="Hello,!")
 
-    # Build the voice request, select the language code ("en-US") and the ssml
-    # voice gender ("neutral")
-    voice = texttospeech.VoiceSelectionParams(
-        language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
-    )
+#     # Build the voice request, select the language code ("en-US") and the ssml
+#     # voice gender ("neutral")
+#     voice = texttospeech.VoiceSelectionParams(
+#         language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+#     )
 
-    # Select the type of audio file you want returned
-    audio_config = texttospeech.AudioConfig(
-        audio_encoding=texttospeech.AudioEncoding.MP3
-    )
+#     # Select the type of audio file you want returned
+#     audio_config = texttospeech.AudioConfig(
+#         audio_encoding=texttospeech.AudioEncoding.MP3
+#     )
 
-    # Perform the text-to-speech request on the text input with the selected
-    # voice parameters and audio file type
-    response = client.synthesize_speech(
-        input=synthesis_input, voice=voice, audio_config=audio_config
-    )
+#     # Perform the text-to-speech request on the text input with the selected
+#     # voice parameters and audio file type
+#     response = client.synthesize_speech(
+#         input=synthesis_input, voice=voice, audio_config=audio_config
+#     )
 
-    # The response's audio_content is binary.
-    with open("output.mp3", "wb") as out:
-        # Write the response to the output file.
-        out.write(response.audio_content)
-        print('Audio content written to file "output.mp3"')
+#     # The response's audio_content is binary.
+#     with open("output.mp3", "wb") as out:
+#         # Write the response to the output file.
+#         out.write(response.audio_content)
+#         print('Audio content written to file "output.mp3"')
     
 
 @socketio.on('socket')
