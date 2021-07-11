d0223f5962f2738b1ab0a599bbee60ebbf3412a9
Benjamin Ng <benymng@gmail.com>
2021-07-11 14:25:39 -0600

    trying web socket

--- server/app.py
+++ server/app.py
@@ -1,11 +1,14 @@
 from flask import Flask, make_response, jsonify, request
 from flask_cors import CORS
+from google.cloud import texttospeech
+from flask_socketio import SocketIO
 
 from functions import scrape, vision
 
 
 app = Flask(__name__)
-app.config.from_object("config")
+socketio = SocketIO(app)
+app.config['SECRET_KEY'] = 'testajdsfkljalsdfjajkdf;ljkasf'
 CORS(app)
 
 
@@ -56,7 +59,47 @@ def recipe():
     recipe_dict['description'] = description
 
     return make_response(jsonify(recipe_dict), 200)
+
+app.route("/text-to-speech", methods=["POST"])
+def textToSpeech():
+    # Instantiates a client
+    client = texttospeech.TextToSpeechClient()
+
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.SynthesisInput(text="Hello,!")
+
+    # Build the voice request, select the language code ("en-US") and the ssml
+    # voice gender ("neutral")
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    )
+
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+
+    # The response's audio_content is binary.
+    with open("output.mp3", "wb") as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
+    
+
+@socketio.on('socket')
+def socket():
+    print("Success")
     
 
 if __name__ == "__main__":
-    app.run()
+    print('[INFO] Starting server at http://localhost:5001')
+    socketio.run(app=app, host='0.0.0.0', port=5001)
+    # app.run()
+
+
